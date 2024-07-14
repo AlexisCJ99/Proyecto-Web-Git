08e5c1d2a98b11883fda0312c5b752e9f9902a06
Alexis CJ <michaeljalexis1999@gmail.com>
2024-07-13 20:31:50 -0500

    MODIFICADO INDEX.JS A APP.JS

---
+++ app.js
@@ -0,0 +1,22 @@
+//AGREGAMOS  LIBRERIAS 
+const express = require("express");
+const router = require('./router/router');
+
+
+//CREAMOS UNA INSTANCIA DE LA APLICAION
+const app = express();
+
+//MIDDLEWARE
+// app.use(express.json());
+app.set('view engine', 'ejs')
+
+//UTILIZANDO RUTAS
+app.use(router);
+
+//CONFIGURACION DEL PUERTO DE ENTRADA
+app.set('port', 4000)
+
+//Ejecucion del servidor web
+app.listen(app.get('port'),() => {
+    console.log("Servidor corriendo en http://localhost:" + app.get('port'));
+})
