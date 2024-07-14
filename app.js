a8d0d1e9b284f2150a8d47d2b2f2df4386ae7e15
Alexis CJ <michaeljalexis1999@gmail.com>
2024-07-14 11:40:58 -0500

    MODIFICANDO EL NAVBAR EN HEADER.EJS

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const router = require('./router/router');
 const app = express();
 
 //MIDDLEWARE
-// app.use(express.json());
 app.set('view engine', 'ejs')
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express(JSON));
+app.use(express.urlencoded({extended : false}));
 
 //UTILIZANDO RUTAS
 app.use(router);
